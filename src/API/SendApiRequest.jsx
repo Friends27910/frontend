fa874312c8b198762e2b2b8c27f565523c582624
Friends27910 <friendsenterprises27910@gmail.com>
2025-08-27 21:04:03 +0530

    first commit

--- src/API/SendApiRequest.jsx
+++ src/API/SendApiRequest.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import React from "react";
 import { isExpired, decodeToken } from "react-jwt";
 import {searchData} from "./StroageApi.js"
-const uri = "http://localhost:2009/api"
+const uri = "https://backend-delta-taupe.vercel.app/Api"
 export const ApiRequest =  async(Data) =>{
   const Data1 = {
     name:Data.Name,
